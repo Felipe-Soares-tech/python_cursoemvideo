4a1ced5429a83c0ff58b137db5f2b9755044888b
felipe soares siqueira <felipesoaressiqueira@gmail.com>
2025-01-27 19:22:39 -0300

    exercicios e criação aula while

--- exercicios/exe060.py
+++ exercicios/exe060.py
@@ -2,4 +2,19 @@
 #
 # 5! = 5 x 4 x 3 x 2 x 1 = 120
 
+multi = 1
 num = int(input('digite um numero: '))
+for i in range (num, 0, -1):
+    if i > 1:
+        print(i, 'x ' ,end= '')
+    else:
+        print(i, '= ', end='')
+    multi *= i
+
+print(multi)
+
+
+
+
+
+
