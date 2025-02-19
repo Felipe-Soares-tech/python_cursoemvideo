e6fa24cdc109008d425d9f32be12ff5668896b07
felipe soares siqueira <felipesoaressiqueira@gmail.com>
2025-02-18 21:47:40 -0300

    lewd32w

---
+++ exercicios/exe076.py
@@ -0,0 +1,7 @@
+# Exercício Python 076: Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
+# No final, mostre uma listagem de preços, organizando os dados em forma tabular.
+
+listagem = ('sapato',90, 'maçã', 1, 'melão', 5, 'alcatra', 100, 'nutella', 25)
+
+for item in listagem:
+    print(item)
